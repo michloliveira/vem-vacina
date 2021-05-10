a1778d1d2c131ab82e7b84a9b668f1f54c1259c1
Michel Oliveira <micheltdoliveira@gmail.com>
2021-05-10 13:04:11 -0300

    add login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'entrar', to:'sessions#new'
+  post 'entrar', to: 'sessions#create'
   resources :vacinas
   resources :usuarios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
