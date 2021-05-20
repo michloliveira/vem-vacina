af34d915b247decc1e1116ddbaabe20afd86cba4
Michel Oliveira <micheltdoliveira@gmail.com>
2021-05-20 13:20:55 -0300

    add validação de hora

--- app/models/vacina.rb
+++ app/models/vacina.rb
@@ -3,4 +3,6 @@ class Vacina < ApplicationRecord
 
     validates :nome,presence: true, length: { minimum: 2}
     validates :descricao,presence: true,length: { minimum:5}
+    validates_date :data,presence: true, on_or_after: lambda { Date.current }
+    validates_time :horario,presence: true, between: ['8:00am', '5:00pm']
 end
