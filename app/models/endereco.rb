6048f1a2fffcfa810522ae0590f7e6336434ec0b
Michel Oliveira <micheltdoliveira@gmail.com>
2021-05-19 23:22:27 -0300

    add validates de endereço

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,9 @@
 class Endereco < ApplicationRecord
   belongs_to :usuario
+
+  validates :cep,presence: true,numericality: { only_integer: true } , length: {is:8}
+  validates :cidade,presence: true, length:{minimum:3}
+  validates :bairro,presence: true
+  validates :logradouro,presence: true
+  validates :complemento,presence: true
 end
